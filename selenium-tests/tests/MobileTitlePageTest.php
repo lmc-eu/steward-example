9e2f2afc4290b0f0fb3ce21defdf67a5f0e8f9b7
Ondřej Machulda <ondrej.machulda@gmail.com>
2015-05-15 00:38:47 +0200

    Show adjustment of default browser size

---
+++ selenium-tests/tests/MobileTitlePageTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace My;
+
+use My\Pages\TitlePage;
+
+/**
+ * Example and simple tests of w3.org title page in mobile mode.
+ * @group responsive
+ */
+class MobileTitlePageTest extends MyAbstractTestCase
+{
+    /** @var int Width of browser window */
+    public static $browserWidth = 320;
+    /** @var int Height of browser window */
+    public static $browserHeight = 480;
+
+    /** @var TitlePage The title-page Page Object */
+    protected $titlePage;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->titlePage = new TitlePage($this);
+        $this->wd->get(self::$baseUrl);
+    }
+
+    public function testShouldNotDisplayLeftSidebarInMobileView()
+    {
+        /** @var \WebDriverDimension $size */
+        $size = $this->wd->manage()->window()->getSize();
+        $this->log('Current screen size is %dx%d px', $size->getWidth(), $size->getHeight());
+
+        $this->assertFalse($this->titlePage->isLeftSidebarVisible());
+    }
+
+    public function testShouldContainGoogleSearchLeadingToGoogleCom()
+    {
+        $this->titlePage->fillAndSubmitSearch('responsive images');
+
+        // The google custom search form should lead us to google search
+        $this->assertContains('www.google', $this->wd->getCurrentURL());
+        $this->assertContains('site:w3.org', $this->wd->getTitle());
+    }
+}
--- selenium-tests/tests/MyAbstractTestCase.php
+++ selenium-tests/tests/MyAbstractTestCase.php
@@ -4,7 +4,15 @@ namespace My;
 
 use Lmc\Steward\Test\AbstractTestCase;
 
+/**
+ * Abstract class for custom tests, could eg. define some properties or instantiate some common components in setUp().
+ */
 abstract class MyAbstractTestCase extends AbstractTestCase
 {
+    /** @var int Default width of browser window (Steward's default is 1280) */
+    public static $browserWidth = 1024;
+    /** @var int Default height of browser window (Steward's default is 1024) */
+    public static $browserHeight = 768;
+    /** @var string */
     public static $baseUrl = 'http://www.w3.org/';
 }
