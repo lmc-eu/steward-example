f77285c650747a6d1fc98c8d514c8bfe5d037b53
Ondřej Machulda <ondrej.machulda@gmail.com>
2017-04-28 21:52:49 +0200

    Add custom capabilities resolver example

---
+++ selenium-tests/src/CustomCapabilitiesResolver.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace My\Steward;
+
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\WebDriverBrowserType;
+use Lmc\Steward\ConfigProvider;
+use Lmc\Steward\Selenium\CustomCapabilitiesResolverInterface;
+use Lmc\Steward\Test\AbstractTestCase;
+
+/**
+ * You can define capabilities for one test run using the `--capability` option of `steward run` command. However,
+ * if you need some custom logic logic or when you need some global capabilities which don't differ between runs,
+ * you can implement the CustomCapabilitiesResolverInterface.
+ *
+ * Then you must specify the Resolver in steward.yml configuration file like this:
+ * `capabilities_resolver: My\Steward\CapabilitiesResolver`
+ *
+ * @see https://github.com/lmc-eu/steward/wiki/Set-custom-capabilities
+ */
+class CustomCapabilitiesResolver implements CustomCapabilitiesResolverInterface
+{
+    /** @var ConfigProvider */
+    private $config;
+
+    public function __construct(ConfigProvider $config)
+    {
+        $this->config = $config;
+    }
+
+    public function resolveDesiredCapabilities(AbstractTestCase $test, DesiredCapabilities $capabilities)
+    {
+        // Capability defined for all test runs
+        $capabilities->setCapability('pageLoadStrategy', 'normal');
+
+        // Capability only for specific browser
+        if ($this->config->browserName === WebDriverBrowserType::IE) {
+            $capabilities->setCapability('ms:someEdgeCapability', 'true');
+        }
+
+        return $capabilities;
+    }
+
+
+    public function resolveRequiredCapabilities(AbstractTestCase $test, DesiredCapabilities $capabilities)
+    {
+        return $capabilities;
+    }
+}
