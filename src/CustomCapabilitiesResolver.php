db87d0e71c36b8406dbdbe728bfd238f9e8066a3
Ondřej Machulda <ondrej.machulda@gmail.com>
2018-01-09 18:58:53 +0100

    Use  as workaround for Chrome crashing on startup on Travis
    https://github.com/SeleniumHQ/selenium/issues/4961

--- src/CustomCapabilitiesResolver.php
+++ src/CustomCapabilitiesResolver.php
@@ -45,7 +45,8 @@ class CustomCapabilitiesResolver implements CustomCapabilitiesResolverInterface
             $chromeOptions = new ChromeOptions();
             // In headless Chrome 60, window size cannot be changed run-time:
             // https://bugs.chromium.org/p/chromium/issues/detail?id=604324#c46
-            $chromeOptions->addArguments(['--headless', 'window-size=1024,768']);
+            // --no-sandbox is workaround for Chrome crashing: https://github.com/SeleniumHQ/selenium/issues/4961
+            $chromeOptions->addArguments(['--headless', 'window-size=1024,768', '--no-sandbox']);
             $capabilities->setCapability(ChromeOptions::CAPABILITY, $chromeOptions);
         }
 
